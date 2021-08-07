b63a05081c0f2c36acd7c5ddb8a3b2e627bd751e
Yota Toyama <raviqqe@gmail.com>
2021-08-07 10:02:30 -0700

    [create-pull-request] automated change (#5)
    Co-authored-by: raviqqe <raviqqe@users.noreply.github.com>

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Pen < Formula
-  version '0.1.13'
+  version '0.1.14'
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '90b05be4241281118ef3fcf42c879635d8079e116e648198372cb3af5e7fd368'
+  sha256 'afc3238f720e1cb8064a34306d19f9e1d69fb970985a4dddf937a1d0e0336654'
   license 'MIT'
 
   conflicts_with 'pen'
