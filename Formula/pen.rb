01ec589277fe3f509a38657e56a0bf368ee5a1d2
raviqqe <raviqqe@users.noreply.github.com>
2021-08-07 15:10:17 +0000

    [create-pull-request] automated change

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -5,7 +5,7 @@ class Pen < Formula
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '90b05be4241281118ef3fcf42c879635d8079e116e648198372cb3af5e7fd368'
+  sha256 '90b05be4241281118ef3fcf42c879635d8079e116e648198372cb3af5e7fd368  -'
   license 'MIT'
 
   conflicts_with 'pen'
