f0e12c3395553af467dcc269fc158138ae948ac4
raviqqe <raviqqe@users.noreply.github.com>
2021-08-07 15:02:36 +0000

    [create-pull-request] automated change

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Pen < Formula
-  version '0.1.13'
+  version ''
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '90b05be4241281118ef3fcf42c879635d8079e116e648198372cb3af5e7fd368'
+  sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  -'
   license 'MIT'
 
   conflicts_with 'pen'
