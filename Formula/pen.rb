910a53331ce927903c45a765359f5416e4667f1b
Yota Toyama <raviqqe@gmail.com>
2022-01-09 02:04:27 -0800

    [create-pull-request] automated change (#54)
    Co-authored-by: mergify[bot] <mergify[bot]@users.noreply.github.com>

--- Formula/pen.rb
+++ Formula/pen.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Pen < Formula
-  version '0.3.9'
+  version '0.3.10'
   desc 'Pen programming language'
   homepage 'https://github.com/pen-lang/pen'
   url "https://github.com/pen-lang/pen/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '9dbe7533ee5d738f53aa9da31ac3f13f8652236d784e6488ffbb07d39ce63d74'
+  sha256 '4316d98f603a16e7a5c0c9b16c7a573fd239c31955fe549f6e821b8dea680f17'
   license 'MIT'
 
   conflicts_with 'pen'
