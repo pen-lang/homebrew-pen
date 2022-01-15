9004f85a4ed23f47ed8eede8d3dbdcc916ece1ef
Yota Toyama <raviqqe@gmail.com>
2022-01-15 03:24:32 +0000

    Add turtle formula

---
+++ Formula/turtle.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Turtle < Formula
+  version '0.0.0'
+  desc 'Ninja-compatible build system for high-level programming languages'
+  homepage 'https://github.com/raviqqe/turtle'
+  url "https://github.com/raviqqe/turtle/archive/refs/tags/v#{version}.tar.gz"
+  sha256 ''
+  license 'MIT'
+
+  conflicts_with 'turtle'
+
+  depends_on 'rust' => :build
+
+  def install
+    system 'cargo', 'install', *std_cargo_args
+  end
+
+  test do
+    system 'turtle', '--version'
+  end
+end
