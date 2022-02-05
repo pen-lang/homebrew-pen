d97ada8a6fe9b7d06254f63cdb365c31bf735022
Yota Toyama <raviqqe@gmail.com>
2022-02-05 10:19:27 -0800

    Update turtle (#64)

--- Formula/turtle.rb
+++ Formula/turtle.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Turtle < Formula
-  version '0.3.0'
+  version '0.3.1'
   desc 'Ninja-compatible build system for high-level programming languages'
   homepage 'https://github.com/raviqqe/turtle'
   url "https://github.com/raviqqe/turtle/archive/refs/tags/v#{version}.tar.gz"
-  sha256 '70d1f0faf9d3b23b94e329adf262844bfe10f14e817bcf350ae260ebf1eb9b4f'
+  sha256 '43b8a132134fa970af2dd736acc6b1918a620870f47be249d34ae82a2895751d'
   license 'MIT'
 
   conflicts_with 'turtle'
