ea0b3486971166e6ca5e07370a09626ae08c6dc5
Yota Toyama <raviqqe@gmail.com>
2022-06-26 20:47:15 +0000

    Bump revision

--- Formula/turtle.rb
+++ Formula/turtle.rb
@@ -6,7 +6,7 @@ class Turtle < Formula
   url "https://github.com/raviqqe/turtle/archive/refs/tags/v0.3.3.tar.gz"
   sha256 "7a71ccaf0edcf4cacd693f2e0ad61855e1b8770e7a9cdc37a22fb5a956b7c4c2"
   license "MIT"
-  revision 2
+  revision 3
 
   depends_on "rust" => :build
 
