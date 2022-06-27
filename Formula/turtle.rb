c6a8b9eb39824d5a18d48a6b55644258cfa5664b
Yota Toyama <9584358+raviqqe@users.noreply.github.com>
2022-06-27 02:22:21 +0000

    turtle: add 0.3.3_3 bottle.

--- Formula/turtle.rb
+++ Formula/turtle.rb
@@ -8,6 +8,12 @@ class Turtle < Formula
   license "MIT"
   revision 3
 
+  bottle do
+    root_url "https://github.com/pen-lang/homebrew-pen/releases/download/turtle-0.3.3_3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "f5392b583711742961ba748295d62199a8b215c5f51ce7c94ac08fbc34c00ffa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cccbfcee750830add96d5bad50338c150c28bdbb1dc88b897eb31180eeaf0d8e"
+  end
+
   depends_on "rust" => :build
 
   def install
