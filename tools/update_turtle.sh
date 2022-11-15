c771f9bede9975c02322f3a11395c080d173e4c5
Yota Toyama <raviqqe@gmail.com>
2022-11-14 22:46:18 -0800

    Update turtle

--- tools/update_turtle.sh
+++ tools/update_turtle.sh
@@ -8,9 +8,10 @@ mkdir -p tmp
 git clone https://github.com/raviqqe/turtle-build tmp/turtle
 
 version=$(cd tmp/turtle && cat Cargo.toml | grep '^version = ' | cut -f 2 -d '"')
+sed=$(which gsed || echo sed)
 
-sed -i "s/version .*/version '$version'/" Formula/turtle.rb
+$sed -i "s%^  url .*%  url \"https://github.com/raviqqe/turtle/archive/refs/tags/v$version.tar.gz\"%" Formula/turtle.rb
 
-hash=$(curl -fsSL https://github.com/raviqqe/turtle-build/archive/refs/tags/v$version.tar.gz | sha256sum | cut -f 1 -d ' ')
+hash=$(curl -fsSL https://github.com/raviqqe/turtle-build/archive/refs/tags/v$version.tar.gz | shasum -a 256 | cut -f 1 -d ' ')
 
-sed -i "s/sha256 .*/sha256 '$hash'/" Formula/turtle.rb
+$sed -i "s/^  sha256 .*/  sha256 \"$hash\"/" Formula/turtle.rb
